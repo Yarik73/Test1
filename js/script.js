f1f3d8addc8d1388e2a37b7e657aa02a522eb516
Yarik73 <artamonovy6@gmail.com>
2020-02-02 16:56:04 +0400

    upd

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function ($) {
-   let $slider = $('.slider-wrap');
-   let $sliderList = $slider.find('ul');
-   let $slideCount = $sliderList.find('li').length;
-   let $slideWidth = $sliderList.find('li').width();
-   let $slideHeight = $sliderList.find('li').height();
-   let $sliderUlWidth = $slideCount * $slideWidth;
+   var $slider = $('.slider-wrap');
+   var $sliderList = $slider.find('ul');
+   var $slideCount = $sliderList.find('li').length;
+   var $slideWidth = $sliderList.find('li').width();
+   var $slideHeight = $sliderList.find('li').height();
+   var $sliderUlWidth = $slideCount * $slideWidth;
 
     $slider.css({width: $slideWidth, height: $slideHeight});
     $sliderList.css({width: $sliderUlWidth, marginLeft: -$slideWidth});
